20574c66b98eadc073816da7364c5e155d099e97
Anirudh Sharma <anirudhsharma885@outlook.com>
2021-04-18 13:31:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+// Navbar
+const navLinks = document.getElementById('navLinks');
+function showMenu(){
+    navLinks.style.right = '0';
+}
+function hideMenu(){
+    navLinks.style.right = '-200px';
+}
+
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//     document.getElementById("nav").style.fontSize = "30px";
+//   } else {
+//     document.getElementById("nav").style.fontSize = "90px";
+//   }
+// }
+
+
+//Courosel Starts here
+
+ document.addEventListener('DOMContentLoaded', function () {
+        new Splide('.splide').mount();
+    });
+
+
+//animation
+
+AOS.init({
+        offset: 250,
+        duration: 1000
+    });
